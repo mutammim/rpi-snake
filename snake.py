96ac43d8621a200dba2ed5f03a6e977531f1b45f
Mutammim <58490040+mutammim@users.noreply.github.com>
2024-11-14 20:06:05 -0500

    Try no anchor

--- snake.py
+++ snake.py
@@ -22,8 +22,8 @@ try:
     title = "Snake"
     subtitle = "Press up to start..."
 
-    draw.text((75, 75), title, font=font, fill="white", anchor="mm")
-    draw.text((100, 100), subtitle, font=font, fill="white", anchor="mm")
+    draw.text((75, 75), title, font=font, fill="white")
+    draw.text((100, 100), subtitle, font=font, fill="white")
     display.LCD_ShowImage(image, 0, 0)
 
     waiting = True
